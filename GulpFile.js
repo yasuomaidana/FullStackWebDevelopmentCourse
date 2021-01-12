7bbc5a3551eac4f5c94bb498fa9e323fce457de2
Yasuo Maidana <a01328427@itesm.mx>
2021-01-12 15:35:07 -0600

    gulp part 2

--- GulpFile.js
+++ GulpFile.js
@@ -2,6 +2,14 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin'),
     browserSync = require('browser-sync');
 
 gulp.task('sass', gulp.series(function () {
@@ -29,7 +37,41 @@ gulp.task('browser-sync', gulp.series(function () {
         });
     
     }));
-    
+
+// Clean
+gulp.task('clean', gulp.series(function() {
+    return del(['dist']);
+}));
+
+gulp.task('copyfonts', gulp.series(async function() {
+   gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+   .pipe(gulp.dest('./dist/fonts'));
+}));
+
+// Images
+gulp.task('imagemin', gulp.series(function() {
+    return gulp.src('img/*.{png,jpg,gif}')
+      .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+      .pipe(gulp.dest('dist/img'));
+  }));
+
+gulp.task('usemin', gulp.series(function() {
+return gulp.src('./*.html')
+.pipe(flatmap(function(stream, file){
+    return stream
+        .pipe(usemin({
+            css: [ rev() ],
+            html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
+            js: [ uglify(), rev() ],
+            inlinejs: [ uglify() ],
+            inlinecss: [ cleanCss(), 'concat' ]
+        }))
+    }))
+    .pipe(gulp.dest('dist/'));
+}));
+
+gulp.task('build', gulp.series('clean',gulp.parallel('copyfonts',
+'imagemin','usemin')));
     // Default task
 gulp.task('default', gulp.parallel('browser-sync','sass:watch'));
       

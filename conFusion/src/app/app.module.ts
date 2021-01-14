33061d0731c914ae5128ad418860d97b4773b811
Yasuo Ignacio Maidana Pérez <yasuomaidana@gmail.com>
2021-01-14 14:37:33 -0600

    component 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -3,22 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//Element added
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
-
+import { MenuComponent } from './menu/menu.component';
+import {MatListModule} from '@angular/material/list'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
+  //Imports again the modules imported, but now they are included in NgModule(s)
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ conFusion/src/app/shared/dish.ts
@@ -0,0 +1,19 @@
+export class Dish { 
+
+    id: string; 
+    
+    name: string; 
+    
+    image: string; 
+    
+    category: string; 
+    
+    featured: boolean; 
+    
+    label: string; 
+    
+    price: string; 
+    
+    description: string; 
+    
+    } 

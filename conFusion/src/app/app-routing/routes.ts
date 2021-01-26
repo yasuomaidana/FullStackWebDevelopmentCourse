8006814972600382034ecc3734b9904b612f4feb
Yasuo Maidana <a01328427@itesm.mx>
2021-01-26 13:10:12 -0600

    url parameters practice

--- conFusion/src/app/app-routing/routes.ts
+++ conFusion/src/app/app-routing/routes.ts
@@ -10,5 +10,6 @@ export const routes: Routes=[
     {path:'home',component:HomeComponent},
     {path:'menu',component:MenuComponent},
     {path:'contact',component:ContactComponent},
+    {path:'dishdetail/:id',component:DishdetailComponent},
     {path:'', redirectTo:'/home', pathMatch:'full'}
 ];
--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+// We use Input when we pass information through methods that 
+//are not links
+//import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+//To use information using url parameters
+import {Params,ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
 import {Dish} from '../shared/dish'
+//Import services
+import {DishService} from '../services/dish.service';
 
 @Component({
   selector: 'app-dishdetail',
@@ -8,10 +16,18 @@ import {Dish} from '../shared/dish'
 })
 export class DishdetailComponent implements OnInit 
 { 
-    @Input()
-     dish = Dish;
-     constructor() { }
+    //Since we are not passing the information through
+    //@Input()
+    dish: Dish;
+    constructor(private dishService:DishService,
+      private location:Location,
+      private route:ActivatedRoute) { }
 
-     ngOnInit() {}
-
+    ngOnInit() {
+      let id = this.route.snapshot.params['id'];
+      this.dish = this.dishService.getDish(id);
+    }
+    goBack(): void{
+      this.location.back();
+    }
 }

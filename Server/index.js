ccd32c3d57f9e227976e0551d69ae3b8618767ab
yasuomaidana <yasuomaidana@gmail.com>
2021-03-26 20:56:02 -0600

    node http example

--- Server/index.js
+++ Server/index.js
@@ -1,43 +1,15 @@
-/*
-rect defined inside
-var rect = {
-	perimeter: (x, y) => (2*(x+y)),
-	area: (x, y) => (x*y)
-};
-*/
+const http = require('http');
 
-// Imports variable
-//const rectangle = require('./rectangle');
-var rect = require('./rectangle');
+const hostname = 'localhost';
+const port = 3000;
 
-/* Using exports
-function solveRect(l,b) {
-    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+const server = http.createServer((req, res) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>Hello, World!</h1></body></html>');
+})
 
-    if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greater than zero:  l = "
-               + l + ",  and b = " + b);
-    }
-    else {
-	    console.log("The area of the rectangle is " + rect.area(l,b));
-	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-    }
-}*/
-
-//Using module
-function solveRect(l,b) {
-    console.log("Solving for rectangle with l = " + l + " and b = " + b);
-    rect(l,b,(err,rectangle)=>{
-        if(err){ console.log("Error for rectangle of l: "+l+" and b: "+b);}
-        else{
-            console.log("For recangle of l:"+l+" and b: "+b);
-            console.log("Area: "+rectangle.area());
-            console.log("Permimeter: "+rectangle.perimeter());
-        }
-    });
-}
-
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

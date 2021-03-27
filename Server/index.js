4fc1c0e7e668f64e1e8c498ce1a4975b20c9f48f
yasuomaidana <yasuomaidana@gmail.com>
2021-03-27 15:37:38 -0600

    Homework 4 building functions with routes and parameters

--- Server/index.js
+++ Server/index.js
@@ -7,65 +7,25 @@ const hostname = "localhost";
 const port = 3000;
 
 const app =express();
+
+//Import routes
+const dishRoutes = require("./routes/dishes/dishRouter");
+const promoRoutes = require("./routes/promotions/promotionsRouter");
+const leaderRoutes = require("./routes/leaders/leaderRouter");
+
 app.use(morgan('dev'));
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 
-///////// For dishes
-app.all('/dishes',(req,res,next)=>{
-  res.statusCode = 200;
-  res.setHeader("Content-Type","text/plain");
-  next();
-});
+//Apply routes
+app.use("/dishes",dishRoutes);
+app.use("/promotions",promoRoutes);
+app.use("/leaders",leaderRoutes);
 
-app.get('/dishes' , (req , res,next)=>{
-   res.end('Will send dishes')
-});
-
-app.post('/dishes' , (req , res,next)=>{
-   res.end('Will add the dish :'+req.body.name + '\n with details :'
-   +req.body.description);
-})
-
-app.put('/dishes',(req,res,next)=>{
-  res.status = 403;
-  res.end("Put operation not supported for dishes");
-});
-
-app.delete('/dishes',(req,res,next)=>{
-  res.status = 403;
- res.end("Delete operation not supported for dishes");
-});
-
-app.get('/dishes/:dishId' , (req , res,next)=>{
-  res.end('Will send details of dish:'+req.params.dishId);
-});
-
-app.post('/dishes/:dishId' , (req , res,next)=>{
-  res.end('Delete operation not supported for dishes/'+req.params.dishId);
-}) 
-
-app.put('/dishes/:dishId',(req,res,next)=>{
- res.write("Updating the dish:"+req.params.dishId+"\n");
- res.end('Will update  the dish :'+req.body.name + '\n with details :'
-   +req.body.description);
-
-});
-
-app.delete('/dishes/:dishId',(req,res,next)=>{
-  res.end('Will delete dish:'+req.params.dishId);
-});
-//////// dishes end
-app.use((req,res,next)=>{
-  //Morgan show something
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-  
-}); 
-
+//Create server
 const server = http.createServer(app);
 
+//Start server
 server.listen(port,hostname,()=>{
   console.log(`Server running at http://${hostname}:${port}/`); 
 });
---
+++ Server/routes/leaders/leaderRouter.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//Import routes
+const leaderIdRoutes = require('./leaederId')
+
+const leaderRouter = express.Router();
+
+leaderRouter.get(bodyParser.json());
+
+leaderRouter.route('/').all((req,res,next)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the leaders to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /leaders');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all leaders');
+});
+
+leaderRouter.use("/",leaderIdRoutes);
+
+module.exports = leaderRouter;

7876d32909db8c99252b60a385766f0e7e64c415
yasuomaidana <yasuomaidana@gmail.com>
2021-04-02 11:29:31 -0600

    basic authentification

--- Server/conFusionServer/app.js
+++ Server/conFusionServer/app.js
@@ -7,6 +7,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Import routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishes/dishRouter');
@@ -16,12 +17,10 @@ var leaderRouter = require('./routes/leaders/leaderRouter');
 //Adds mongoose stuffs
 const mongoose = require("mongoose");
 
-//Import models
-const Dishes = require("./models/dishes");
-
 //Url Stuffs
 const url = "mongodb://localhost:27017/conFusion";
 
+//Connect to database
 const connect = mongoose.connect(url);
 
 connect.then(db=>{
@@ -39,6 +38,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Autentication stuffs
+function auth(req,res,next){
+  console.log(req.headers);
+  
+  var authHeaders = req.headers.authorization;
+  if (!authHeaders){
+    var err = new Error("You are not authenticated");
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status = 401;
+    return next(err);
+  }
+  
+  var auth = new Buffer.from(authHeaders.split(' ')[1],'base64').toString().split(':');
+  var username = auth[0];
+  var password = auth[1];
+
+  if(username === 'admin' && password === 'password'){
+    next();
+  }
+  else{
+   var err = new Error("You are not authenticated");
+   res.setHeader('WWW-Authenticate','Basic');
+   err.status = 401;
+   return next(err);   
+  }
+}
+app.use(auth);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
